928344a5cc4db4bf89a3367dafa6685153bab566
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-02 20:10:10 +0530

    design the ui sidebar and homepage design

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,7 +1,100 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
+import assets from "../../assets";
+import { Box } from "@mui/system";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useNavigate } from "react-router-dom";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const sidebarWidth = 250;
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{
+        width: sidebarWidth,
+        height: "100vh",
+        "& > div": { borderRight: "none" },
+      }}
+    >
+      <List
+        disablePadding
+        sx={{
+          width: sidebarWidth,
+          height: "100vh",
+          backgroundColor: assets.colors.secondary,
+        }}
+      >
+        <ListItem>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              {user.username}
+            </Typography>
+            <IconButton onClick={logout}>
+              <LogoutIcon fontSize="small" />
+            </IconButton>
+          </Box>
+        </ListItem>
+        <Box sx={{ paddingTop: "10px" }} />
+        <ListItem>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              Favourites
+            </Typography>
+          </Box>
+        </ListItem>
+        <Box sx={{ paddingTop: "10px" }} />
+        <ListItem>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              Private
+            </Typography>
+            <IconButton>
+              <AddBoxOutlinedIcon fontSize="small" />
+            </IconButton>
+          </Box>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 };
 
 export default Sidebar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Box } from "@mui/system";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  const createBoard = () => {};
+
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <LoadingButton
+        variant="outlined"
+        color="success"
+        onClick={createBoard}
+        loading={loading}
+      >
+        Click here to create your first board
+      </LoadingButton>
+    </Box>
+  );
 };
 
 export default Home;
